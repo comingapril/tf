458ed75dbc4ff808871e0210c8630f45d55e749e
tappetanavi <naveen.tappeta3@gmail.com>
2023-06-28 21:56:11 +0530

    added

--- ntier/aws/inputs.tf
+++ ntier/aws/inputs.tf
@@ -14,3 +14,13 @@ variable "ntier-subnet-cidrs" {
   type = list(string)
   default = [ "192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24" ] 
 }
+
+variable "ntier-subnet-azs" {
+  type = list(string)
+  default = [ "a", "b", "a", "b" ]
+}
+
+variable "ntier-subnet-names" {
+  type = list(string)
+  default = [ "app1", "app2", "db1", "db2" ]
+}
--- ntier/aws/main.tf
+++ ntier/aws/main.tf
@@ -9,11 +9,11 @@ resource "aws_vpc" "ntier" {
 resource "aws_subnet" "subnets" {
     count = 4
   cidr_block = var.ntier-subnet-cidrs[count.index]
-  availability_zone = "${var.region}a"
+  availability_zone = "${var.region}${var.ntier-subnet-azs[count.index]}"
   vpc_id = aws_vpc.ntier.id  #implicit dependency
-  tags = {
-        Name = "app1"
-    }
   depends_on = [ aws_vpc.ntier ]
+  tags = {
+    Name = var.ntier-subnet-names[count.index]
+  }
 }
 
