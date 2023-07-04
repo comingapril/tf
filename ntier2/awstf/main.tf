678c93f1070a32cf6d5dadeaa15a57f8bd88b35c
tappetanavi <naveen.tappeta3@gmail.com>
2023-07-04 22:44:01 +0530

    added

--- ntier2/awstf/main.tf
+++ ntier2/awstf/main.tf
@@ -9,19 +9,35 @@ resource "aws_vpc" "ntier" {
 resource "aws_subnet" "app1" {
   cidr_block = var.ntier_app1_cidr
   availability_zone = "{var.region}a"
+  vpc_id = aws_vpc.ntier.id
+  depends_on = [ 
+    aws_vpc.ntier
+   ]
 }
 
 resource "aws_subnet" "app2" {
   cidr_block = var.ntier_app2_cidr
   availability_zone = "{var.region}b"
+  vpc_id = aws_vpc.ntier.id
+  depends_on = [ 
+    aws_vpc.ntier
+   ]
 }
 
 resource "aws_subnet" "db1" {
   cidr_block = var.ntier_db1_cidr
   availability_zone = "{var.region}a"
+  vpc_id = aws_vpc.ntier.id
+  depends_on = [ 
+    aws_vpc.ntier
+   ]
 }
 
 resource "aws_subnet" "db2" {
   cidr_block = var.ntier_db2_cidr
   availability_zone = "{var.region}b"
+  vpc_id = aws_vpc.ntier.id
+  depends_on = [ 
+    aws_vpc.ntier
+   ]
 }
