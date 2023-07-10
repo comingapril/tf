d6366656c63657b07f43af4d10c333cbf44522b0
tappetanavi <naveen.tappeta3@gmail.com>
2023-07-10 14:13:25 +0530

    added

--- ntier2/awstf/database.tf
+++ ntier2/awstf/database.tf
@@ -13,3 +13,20 @@ resource "aws_security_group" "db" {
   depends_on = [ aws_subnet.subnets ]
 }
 
+data "aws_subnet" "db" {
+  filter {
+    name = "tag:Name"
+    values = var.ntier_vpc_info.db_subnets
+  }
+   filter {
+    name = "vpc-id"
+    values = [local.vpc_id]
+  }
+  depends_on = [ aws_subnet.subnets ]
+}
+
+data "aws_db_subnet_group" "ntier" {
+  name = "ntier"
+  subnet_ids = data.aws_subnets.db.ids 
+  depends_on = [ aws_subnet.subnets ]
+}
--- ntier2/awstf/inputs.tf
+++ ntier2/awstf/inputs.tf
@@ -11,6 +11,7 @@ variable "ntier_vpc_info" {
     subnet_names = list(string)
     private_subnets = list(string)
     public_subnets = list(string)
+    db_subnets = list(string)
   })
   default = {
     subnet_azs = [ "a", "b", "a", "b" ]
@@ -18,6 +19,7 @@ variable "ntier_vpc_info" {
     vpc_cidr = "192.168.0.0/16"
     public_subnets = [  ]
     private_subnets = [ "app1", "app2", "db1", "db2" ]
+    db_subnets = [ "db1", "db2" ]
   }
 }
 
