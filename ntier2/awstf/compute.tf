ee99d03fb1a10e846c67e2994141804409f146db
tappetanavi <naveen.tappeta3@gmail.com>
2023-07-11 15:21:48 +0530

    added

---
+++ ntier2/awstf/compute.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "web" {
+   name = "mysql"
+  ingress {
+ from_port = local.ssh_port
+ to_port = local.ssh_port
+ cidr_blocks = [ local.anywhere ]
+ protocol = local.tcp
+  }
+  ingress {
+ from_port = local.http_port
+ to_port = local.http_port
+ cidr_blocks = [ local.anywhere ]
+ protocol = local.tcp
+  }
+  tags = {
+    Name = "web"
+  }
+  vpc_id = local.vpc_id
+  depends_on = [ aws_subnet.subnets ]
+}
