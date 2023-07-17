d0ce74f61430680442adfcab193c7d849f0232b3
tappetanavi <naveen.tappeta3@gmail.com>
2023-07-17 15:38:43 +0530

    added

--- lp/count/inputs.tf
+++ lp/count/inputs.tf
@@ -3,5 +3,20 @@ variable "filenames" {
   default = [ "test1.txt", "test2.txt", "test3.txt" ]
 }
 
-
+variable "fileinfo" {
+  type = map(object({
+    name = string
+    content = string
+  }))
+  default = {
+    "first" = {
+      content = "first"
+      name = "1.txt"
+    },
+    "second" = {
+      content = "second"
+      name = "2.txt"
+    }
+  }
+}
 
--- lp/count/main.tf
+++ lp/count/main.tf
@@ -1,7 +1,7 @@
 resource "local_file" "foo" {
- for_each = toset(var.filenames)
-  content = "hello"
-  filename = each.key
+ for_each = var.fileinfo
+  content = each.value.content
+  filename = each.key.name
 }
 
 
