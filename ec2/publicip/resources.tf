ff403b1dc4fd89ccafec07f7d1c54bd134991406
tappetanavi <naveen.tappeta3@gmail.com>
2023-07-18 17:23:56 +0530

    added

--- ec2/publicip/resources.tf
+++ ec2/publicip/resources.tf
@@ -6,5 +6,18 @@ resource "aws_instance" "apache" {
   subnet_id = data.aws_subnet.first.id
   user_data = file("apache.sh")
   vpc_security_group_ids = ["sg-03d0b2194da305f89"]
+  connection {
+    host = self.public_ip
+    user = "ububtu"
+    private_key = file("~/.ssh/id_rsa")
+    type = "ssh"
+  }
+  provisioner "file" {
+    source = "apache.sh"
+    destination = "/tmp/apache.sh"
+  }
+  provisioner "remote-exec" {
+    script = "/tmp/apache.sh"
+  }
 }
 
